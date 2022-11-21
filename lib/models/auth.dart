47f1c13e5251765a02869e780f3365f7a40d69e6
tiwaojo <tiwaojo@gmail.com>
2022-11-21 05:12:48 -0500

    fix: googleOAuth not fully functioning. Shared Prefs not working properly.
    Signed-off-by: tiwaojo <tiwaojo@gmail.com>

--- lib/models/auth.dart
+++ lib/models/auth.dart
@@ -3,6 +3,9 @@ import 'dart:convert';
 import 'package:extension_google_sign_in_as_googleapis_auth/extension_google_sign_in_as_googleapis_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
+
+t';
+
 /// Provides the `GoogleSignIn` class
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:googleapis/calendar/v3.dart';
