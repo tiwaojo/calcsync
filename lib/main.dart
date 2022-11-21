daa45acca8ace9f63f3e3c531efe5b98315cddf8
Preet Patel <preet.patel2@ontariotechu.net>
2022-11-20 23:03:48 -0500

    Adding Calendar View

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:calsync/auth/calsync_auth.dart';
-import 'package:calsync/events/read_events.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
