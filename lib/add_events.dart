3df3f98754997db144419fd3b84b1ee64a9e583d
Aaditya Rajput <aadiraj09@yahoo.com>
2022-11-21 23:22:59 -0500

    Implementing firebase in the form. Signed-off-by: Aaditya Rajput <aadiraj09@yahoo.com>
    Signed-off-by: Aaditya Rajput <aadiraj09@yahoo.com>

--- lib/add_events.dart
+++ lib/add_events.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
+import '../db/firestore_crud.dart';
+import '../db/firestore.dart';
+import '../models/event.dart';
 
 class add_events extends StatelessWidget {
   @override
@@ -17,9 +20,12 @@ class addEvent extends StatefulWidget {
 }
 
 class _addEventState extends State<addEvent> {
-  DateTime day = DateTime.now();
-
-  TimeOfDay time = TimeOfDay.now();
+  String title = '';
+  String discription = '';
+  DateTime start_day = DateTime.now();
+  TimeOfDay start_time = TimeOfDay.now();
+  DateTime end_day = DateTime.now();
+  TimeOfDay end_time = TimeOfDay.now();
 
   @override
   Widget build(BuildContext context) {
@@ -32,50 +38,131 @@ class _addEventState extends State<addEvent> {
         child: Column(children: [
           TextFormField(
             decoration: InputDecoration(hintText: 'Title'),
+            onChanged: (value) => setState(() => title = value),
           ),
           TextFormField(
             decoration: InputDecoration(hintText: 'Description'),
+            onChanged: (value) => setState(() => discription = value),
           ),
-          Text("Select the time of your Reminder"),
+          Text("Select the start time of your event"),
           ListTile(
-            title: Text("Date: ${day.year}, ${day.month}, ${day.day}"),
+            title: Text(
+                "Date: ${start_day.year}, ${start_day.month}, ${start_day.day}"),
             trailing: Icon(Icons.calendar_today),
             onTap: () {
-              _pickDate(context);
+              _pickStartDate(context);
             },
           ),
           ListTile(
-            title: Text("Time: ${time.hour}:${time.minute}"),
+            title: Text("Time: ${start_time.hour}:${start_time.minute}"),
             trailing: Icon(Icons.access_time),
             onTap: () {
-              _pickTime(context);
+              _pickStartTime(context);
             },
-          )
+          ),
+          Text("Select the end time of your event"),
+          ListTile(
+            title:
+                Text("Date: ${end_day.year}, ${end_day.month}, ${end_day.day}"),
+            trailing: Icon(Icons.calendar_today),
+            onTap: () {
+              _pickEndDate(context);
+            },
+          ),
+          ListTile(
+            title: Text("Time: ${end_time.hour}:${end_time.minute}"),
+            trailing: Icon(Icons.access_time),
+            onTap: () {
+              _pickEndTime(context);
+            },
+          ),
+          ElevatedButton(
+              onPressed: () {
+                create_event(title, discription, start_day, start_time, end_day,
+                    end_time);
+              },
+              child: Text("Create Event")),
         ]),
       ),
     );
   }
 
-  Future<void> _pickDate(BuildContext context) async {
+  Future<void> _pickStartDate(BuildContext context) async {
     final DateTime? selectedDate = await showDatePicker(
         context: context,
-        initialDate: day,
+        initialDate: DateTime.now(),
         firstDate: DateTime(2000),
         lastDate: DateTime(2100));
     if (selectedDate != null) {
       setState(() {
-        day = selectedDate;
+        start_day = selectedDate;
       });
     }
   }
 
-  Future<void> _pickTime(BuildContext context) async {
-    final TimeOfDay? t =
-        await showTimePicker(context: context, initialTime: time);
-    if (t != null) {
+  Future<void> _pickEndDate(BuildContext context) async {
+    final DateTime? selectedDate = await showDatePicker(
+        context: context,
+        initialDate: start_day,
+        firstDate: start_day,
+        lastDate: DateTime(2100));
+    if (selectedDate != null) {
       setState(() {
-        time = t;
+        end_day = selectedDate;
       });
     }
+  }
+
+  Future<void> _pickStartTime(BuildContext context) async {
+    final TimeOfDay? t =
+        await showTimePicker(context: context, initialTime: start_time);
+    if (t != null) {
+      setState(() {
+        start_time = t;
+      });
+    }
+  }
+
+  Future<void> _pickEndTime(BuildContext context) async {
+    final TimeOfDay? t =
+        await showTimePicker(context: context, initialTime: start_time);
+    if (t != null) {
+      setState(() {
+        end_time = t;
+      });
+    }
+  }
+
+  void create_event(String title, String discription, DateTime start_day,
+      TimeOfDay start_time, DateTime end_day, TimeOfDay end_time) {
+    DateTime to = DateTime(start_day.year, start_day.month, start_day.day,
+        start_time.hour, start_time.minute);
+    DateTime from = DateTime(end_day.year, end_day.month, end_day.day,
+        end_time.hour, end_time.minute);
+    // new_event.name = title;
+    // new_event.description = discription;
+    // new_event.from = start_day;
+    // new_event.to = end_day;
+
+    FirestoreCrud.addEvent(
+        to: to,
+        from: from,
+        isAllDay: false,
+        name: title,
+        description: discription,
+        email: "test@test.com");
+
+    showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+                title: Text("Created"),
+                content: Text("$title - $discription \n"
+                    " From Date: ${start_day.year}, ${start_day.month}, ${start_day.day} Time: ${start_time.hour}:${start_time.minute} \n "
+                    " To Date: ${end_day.year}, ${end_day.month}, ${end_day.day} Time: ${end_time.hour}:${end_time.minute}"),
+                actions: [
+                  TextButton(
+                      onPressed: () => Navigator.pop(context),
+                      child: Text("OK"))
+                ]));
   }
 }
