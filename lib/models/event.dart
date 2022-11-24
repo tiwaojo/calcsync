40c88e7616df8c404a756c73d27aeaf0f595d029
Preet Patel <preet.patel2@ontariotechu.net>
2022-11-24 17:42:41 -0500

    Added SQLite for local database

--- lib/models/event.dart
+++ lib/models/event.dart
@@ -2,7 +2,6 @@ class Event {
   String? id;
   DateTime? from;
   DateTime? to;
-  bool? isAllDay;
   String? name;
   String? description;
   String? email;
@@ -11,8 +10,26 @@ class Event {
       {required this.id,
       required this.from,
       required this.to,
-      required this.isAllDay,
       required this.name,
       required this.description,
       required this.email});
+
+  Event.fromMap(Map<String, dynamic> item)
+      : id = item["id"],
+        description = item["description"],
+        name = item["name"],
+        email = item["email"],
+        from = DateTime.parse(item["from"]),
+        to = DateTime.parse(item["from"]);
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'description': description,
+      'name': name,
+      'email': email,
+      'from': from.toString(),
+      'to': to.toString()
+    };
+  }
 }
