2e886213311e5362b0e043190ed99db31e5e3bd1
tiwaojo <tiwaojo@gmail.com>
2022-11-25 10:18:04 -0500

    style: updated styles and integrated onboarding screens.
    feat: added signout

--- lib/views/settings_page.dart
+++ lib/views/settings_page.dart
@@ -1,7 +1,10 @@
 import 'package:calendar_sync/views/day_page.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../auth/calsync_auth.dart';
+import '../models/auth.dart';
 import '../settings.dart';
 
 class SettingsPage extends StatefulWidget {
@@ -15,26 +18,43 @@ class _SettingsPageState extends State<SettingsPage> {
   bool dispPage = false;
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      scrollDirection: Axis.vertical,
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          // CalsyncAuth(),
-          ElevatedButton(
-            onPressed: () {
-              dispPage = true;
-              setState(() {
-                print("Render Page: $dispPage");
-              });
-            },
-            child: const Text("Sync"),
+    return Consumer<CalsyncGoogleOAuth>(
+      builder: (BuildContext context, notifier, child) {
+        return SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // CalsyncAuth(),
+              ElevatedButton(
+                onPressed: () {
+                  dispPage = true;
+                  setState(() {
+                    print("Render Page: $dispPage");
+                  });
+                },
+                child: const Text("Sync"),
+              ),
+              ElevatedButton(
+                  onPressed: () async {
+                    SharedPreferences _prefs =
+                        await SharedPreferences.getInstance();
+                    _prefs.setBool(
+                        "navToHome", false); // Set onboarding to false
+
+                    print(notifier.getCurrentUser);
+                    notifier.handleSignOut(); // sign out user
+                    print(notifier.getCurrentUser);
+                    Navigator.pop(context);
+                  },
+                  child: Text("Signout")),
+              dispPage ? DayPage() : Text("Could not display day page"),
+              Settings(),
+            ],
           ),
-          dispPage ? DayPage() : Text("Could not display day page"),
-          Settings(),
-        ],
-      ),
+        );
+      },
     );
   }
 }
